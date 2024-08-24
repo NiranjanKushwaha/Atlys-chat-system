1c3e2554170c750a631ed4d4198697d05c165333
NiranjanKushwaha <0niranjankumar1@gmail.com>
2024-08-24 10:56:18 +0530

    Login component ui enahanced and on password show toggle feature implemented

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { IPostData } from "../interfaces/general.interface";
 import { chatBubble } from "../assets";
+import { CommentsSvg } from "../utils/GetSvg";
 
 interface PostProps extends IPostData {
   commentsCount: number;
@@ -51,18 +52,8 @@ const Post: React.FC<PostProps> = ({ posterName, posterImage, timeAgo, content, 
         <div className="p-2">{content}</div>
       </div>
       <div className=" flex items-center text-gray-500 text-sm mb-4">
-        <div className="ml-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-chat-left hover-effect"
-            viewBox="0 0 16 16"
-            onClick={handleCommentsVisibility}
-          >
-            <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
-          </svg>
+        <div className="ml-2" onClick={handleCommentsVisibility}>
+          <CommentsSvg/>
         </div>
         <div className="ml-3">{commentsCount} comments</div>
       </div>
